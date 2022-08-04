606a942b5dd05eeb6fa547f780534e70b24c5aca
furvietto <fulvio-94@hotmail.it>
2022-08-04 16:16:06 +0200

    add get All to institute

---
+++ institute-service/src/main/java/com/clan/instaclass/instituteService/models/institute/GetAllInstituteResponse.java
@@ -0,0 +1,10 @@
+package com.clan.instaclass.instituteService.models.institute;
+
+import lombok.Data;
+
+@Data
+public class GetAllInstituteResponse {
+    private Integer id;
+    private String username;
+    private String name;
+}
