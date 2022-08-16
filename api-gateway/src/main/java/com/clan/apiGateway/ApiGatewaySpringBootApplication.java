839d3697313db3b6f66ea502a98f435262026a5c
furvietto <fulvio-94@hotmail.it>
2022-08-16 15:01:41 +0200

    add api gateway

--- api-gateway/src/main/java/com/clan/apiGateway/ApiGatewaySpringBootApplication.java
+++ api-gateway/src/main/java/com/clan/apiGateway/ApiGatewaySpringBootApplication.java
@@ -1,15 +1,19 @@
-package com.clan.instaclass.instituteService;
+package com.clan.apiGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
 @EnableEurekaClient
-public class InsituteApplication {
+public class ApiGatewaySpringBootApplication {
     public static void main(String[] args) {
-        SpringApplication.run(InsituteApplication.class, args);
+        try {
+            SpringApplication.run(ApiGatewaySpringBootApplication.class , args);
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- class-service/src/main/java/com/clan/instaclass/classService/ClassApplication.java
+++ class-service/src/main/java/com/clan/instaclass/classService/ClassApplication.java
@@ -2,10 +2,12 @@ package com.clan.instaclass.classService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableEurekaClient
 public class ClassApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClassApplication.class, args);
