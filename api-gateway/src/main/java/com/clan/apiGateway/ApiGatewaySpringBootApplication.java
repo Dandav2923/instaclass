71c76c52f9737dde14dd09acf1d4a422097745fc
furvietto <fulvio-94@hotmail.it>
2022-08-29 15:50:01 +0200

    add hystrix and circuitBreaker

--- api-gateway/src/main/java/com/clan/apiGateway/ApiGatewaySpringBootApplication.java
+++ api-gateway/src/main/java/com/clan/apiGateway/ApiGatewaySpringBootApplication.java
@@ -3,10 +3,12 @@ package com.clan.apiGateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 public class ApiGatewaySpringBootApplication {
     public static void main(String[] args) {
         try {
